52bef25f457c6a99d29598349ab265611f058da4
mcspe <mcspezzaferro@gmail.com>
2023-06-20 17:07:49 +0200

    [add] museums migration

---
+++ database/migrations/2023_06_20_145758_create_museums_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('museums', function (Blueprint $table) {
+            $table->id();
+            $table->string('museum_name');
+            $table->decimal('latitude', $precision = 9, $scale = 6)->nullable();
+            $table->decimal('longitude', $precision = 9, $scale = 6)->nullable();
+            $table->string('city');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('museums');
+    }
+};
