6bfa7c72e81a84b6339d694e2a2d3587133e2ec2
mcspe <mcspezzaferro@gmail.com>
2023-06-22 18:00:21 +0200

    [add] save minor edits

--- database/seeders/ArtistsTableSeeder.php
+++ database/seeders/ArtistsTableSeeder.php
@@ -22,7 +22,7 @@ class ArtistsTableSeeder extends Seeder
             $new_artist = New Artist;
 
             $new_artist->artist_name = $faker->name();
-            $new_artist->slug = FunctionHelper::generateUniqueSlug($new_artist->artist_name, New Artist);
+            $new_artist->slug = FunctionHelper::generateUniqueSlug($new_artist->artist_name, New Artist());
             $new_artist->artist_lastname = $faker->lastName();
             $new_artist->date_of_birth = $faker->date();
             $new_artist->place_of_birth = $faker->city();
